08ea26f740636df11733e20a67210fda0dc26957
Marijo Knez <c-mknez@neogov.net>
2024-08-29 16:39:47 +0200

    brainsorming

--- Logistics/Logistics.Domain.Import/Transport.cs
+++ Logistics/Logistics.Domain.Import/Transport.cs
@@ -3,14 +3,13 @@
     public class Transport
     {
         internal int Id { get; private set; }
-        internal string VehicleRegistrationNumber { get; private set; }
         internal int MaxLoadMass { get; private set; }
         internal int CurrentLoadMass { get; private set; }
 
         private ICollection<Terminal> route;        
-        public Transport(string vehicleRegistrationNumber, int maxLoadMass, int currentLoadMass, ICollection<Terminal> route)
+        public Transport(int id, int maxLoadMass, int currentLoadMass, ICollection<Terminal> route)
         {
-            VehicleRegistrationNumber = vehicleRegistrationNumber;
+            Id = id;
             MaxLoadMass = maxLoadMass;
             CurrentLoadMass = currentLoadMass;
             this.route = route;
